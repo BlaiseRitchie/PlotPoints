e7427aab974b22a3088037aff673a0318ae74325
Blaise Ritchie <britchie@ellumus.com>
2015-09-28 11:44:13 -0700

    Imported ExceptionForm properly

--- Earth2Revit/ExceptionForm.Designer.cs
+++ Earth2Revit/ExceptionForm.Designer.cs
@@ -1,154 +1,154 @@
 ﻿namespace Earth2Revit {
-  partial class ExceptionForm {
-    /// <summary>
-    /// Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
+    partial class ExceptionForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-    /// <summary>
-    /// Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing) {
-      if(disposing && (components != null)) {
-        components.Dispose();
-      }
-      base.Dispose(disposing);
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.dontSendButton = new System.Windows.Forms.Button();
+            this.viewReportButton = new System.Windows.Forms.Button();
+            this.commentsTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "An unhandled exception occurred.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "A report has been generated.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Would you like to send it?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(221, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Additional comments:(e.g. reproduction steps)";
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(12, 250);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(70, 23);
+            this.sendButton.TabIndex = 5;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // dontSendButton
+            // 
+            this.dontSendButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dontSendButton.Location = new System.Drawing.Point(88, 250);
+            this.dontSendButton.Name = "dontSendButton";
+            this.dontSendButton.Size = new System.Drawing.Size(75, 23);
+            this.dontSendButton.TabIndex = 6;
+            this.dontSendButton.Text = "Don\'t Send";
+            this.dontSendButton.UseVisualStyleBackColor = true;
+            this.dontSendButton.Click += new System.EventHandler(this.dontSendButton_Click);
+            // 
+            // viewReportButton
+            // 
+            this.viewReportButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.viewReportButton.Location = new System.Drawing.Point(169, 250);
+            this.viewReportButton.Name = "viewReportButton";
+            this.viewReportButton.Size = new System.Drawing.Size(83, 23);
+            this.viewReportButton.TabIndex = 7;
+            this.viewReportButton.Text = "View Report";
+            this.viewReportButton.UseVisualStyleBackColor = true;
+            this.viewReportButton.Click += new System.EventHandler(this.viewReportButton_Click);
+            // 
+            // commentsTextBox
+            // 
+            this.commentsTextBox.AcceptsReturn = true;
+            this.commentsTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                    | System.Windows.Forms.AnchorStyles.Right)));
+            this.commentsTextBox.Location = new System.Drawing.Point(12, 94);
+            this.commentsTextBox.Multiline = true;
+            this.commentsTextBox.Name = "commentsTextBox";
+            this.commentsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.commentsTextBox.Size = new System.Drawing.Size(240, 150);
+            this.commentsTextBox.TabIndex = 8;
+            // 
+            // ExceptionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 285);
+            this.Controls.Add(this.commentsTextBox);
+            this.Controls.Add(this.viewReportButton);
+            this.Controls.Add(this.dontSendButton);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ExceptionForm";
+            this.Text = "Unhandled Exception";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ExceptionForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Button dontSendButton;
+        private System.Windows.Forms.Button viewReportButton;
+        private System.Windows.Forms.TextBox commentsTextBox;
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent() {
-      this.label1 = new System.Windows.Forms.Label();
-      this.label2 = new System.Windows.Forms.Label();
-      this.label3 = new System.Windows.Forms.Label();
-      this.label4 = new System.Windows.Forms.Label();
-      this.sendButton = new System.Windows.Forms.Button();
-      this.dontSendButton = new System.Windows.Forms.Button();
-      this.viewReportButton = new System.Windows.Forms.Button();
-      this.commentsTextBox = new System.Windows.Forms.TextBox();
-      this.SuspendLayout();
-      // 
-      // label1
-      // 
-      this.label1.AutoSize = true;
-      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-      this.label1.Location = new System.Drawing.Point(12, 9);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(230, 18);
-      this.label1.TabIndex = 0;
-      this.label1.Text = "An unhandled exception occurred.";
-      // 
-      // label2
-      // 
-      this.label2.AutoSize = true;
-      this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label2.Location = new System.Drawing.Point(12, 27);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(197, 18);
-      this.label2.TabIndex = 1;
-      this.label2.Text = "A report has been generated.";
-      // 
-      // label3
-      // 
-      this.label3.AutoSize = true;
-      this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label3.Location = new System.Drawing.Point(12, 45);
-      this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(177, 18);
-      this.label3.TabIndex = 2;
-      this.label3.Text = "Would you like to send it?";
-      // 
-      // label4
-      // 
-      this.label4.AutoSize = true;
-      this.label4.Location = new System.Drawing.Point(12, 78);
-      this.label4.Name = "label4";
-      this.label4.Size = new System.Drawing.Size(221, 13);
-      this.label4.TabIndex = 3;
-      this.label4.Text = "Additional comments:(e.g. reproduction steps)";
-      // 
-      // sendButton
-      // 
-      this.sendButton.Location = new System.Drawing.Point(12, 250);
-      this.sendButton.Name = "sendButton";
-      this.sendButton.Size = new System.Drawing.Size(70, 23);
-      this.sendButton.TabIndex = 5;
-      this.sendButton.Text = "Send";
-      this.sendButton.UseVisualStyleBackColor = true;
-      this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
-      // 
-      // dontSendButton
-      // 
-      this.dontSendButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
-      this.dontSendButton.Location = new System.Drawing.Point(88, 250);
-      this.dontSendButton.Name = "dontSendButton";
-      this.dontSendButton.Size = new System.Drawing.Size(75, 23);
-      this.dontSendButton.TabIndex = 6;
-      this.dontSendButton.Text = "Don\'t Send";
-      this.dontSendButton.UseVisualStyleBackColor = true;
-      this.dontSendButton.Click += new System.EventHandler(this.dontSendButton_Click);
-      // 
-      // viewReportButton
-      // 
-      this.viewReportButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-      this.viewReportButton.Location = new System.Drawing.Point(169, 250);
-      this.viewReportButton.Name = "viewReportButton";
-      this.viewReportButton.Size = new System.Drawing.Size(83, 23);
-      this.viewReportButton.TabIndex = 7;
-      this.viewReportButton.Text = "View Report";
-      this.viewReportButton.UseVisualStyleBackColor = true;
-      this.viewReportButton.Click += new System.EventHandler(this.viewReportButton_Click);
-      // 
-      // commentsTextBox
-      // 
-      this.commentsTextBox.AcceptsReturn = true;
-      this.commentsTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-      this.commentsTextBox.Location = new System.Drawing.Point(12, 94);
-      this.commentsTextBox.Multiline = true;
-      this.commentsTextBox.Name = "commentsTextBox";
-      this.commentsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-      this.commentsTextBox.Size = new System.Drawing.Size(240, 150);
-      this.commentsTextBox.TabIndex = 8;
-      // 
-      // ExceptionForm
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(264, 285);
-      this.Controls.Add(this.commentsTextBox);
-      this.Controls.Add(this.viewReportButton);
-      this.Controls.Add(this.dontSendButton);
-      this.Controls.Add(this.sendButton);
-      this.Controls.Add(this.label4);
-      this.Controls.Add(this.label3);
-      this.Controls.Add(this.label2);
-      this.Controls.Add(this.label1);
-      this.MaximizeBox = false;
-      this.MinimizeBox = false;
-      this.Name = "ExceptionForm";
-      this.Text = "Unhandled Exception";
-      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ExceptionForm_FormClosing);
-      this.ResumeLayout(false);
-      this.PerformLayout();
-
-    }
-
-    #endregion
-
-    private System.Windows.Forms.Label label1;
-    private System.Windows.Forms.Label label2;
-    private System.Windows.Forms.Label label3;
-    private System.Windows.Forms.Label label4;
-    private System.Windows.Forms.Button sendButton;
-    private System.Windows.Forms.Button dontSendButton;
-    private System.Windows.Forms.Button viewReportButton;
-    private System.Windows.Forms.TextBox commentsTextBox;
-  }
 }
